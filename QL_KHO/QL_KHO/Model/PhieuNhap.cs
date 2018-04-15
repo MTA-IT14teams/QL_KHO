8b924795beb44921c7fc8f88782971cf48ab918c
Thuy Chinh <->
2018-04-15 21:25:14 +0700

    Model_PhieuNhap_PhieuXuat

---
+++ QL_KHO/QL_KHO/Model/PhieuNhap.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QL_KHO.Model
+{
+    class PhieuNhap
+    {
+    }
+}
---
+++ QL_KHO/QL_KHO/Model/PhieuXuat.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QL_KHO.Model
+{
+    class PhieuXuat
+    {
+        private string maPX;
+        private DateTime ngayXuat;
+        private float tongTien;
+        public string MaPX
+        {
+            get
+            {
+                return maPX;
+            }
+
+            set
+            {
+                maPX = value;
+            }
+        }
+        public DateTime NgayXuat
+        {
+            get
+            {
+                return ngayXuat;
+            }
+
+            set
+            {
+                ngayXuat = value;
+            }
+        }
+        public float TongTien
+        {
+            get
+            {
+                return tongTien;
+            }
+
+            set
+            {
+                tongTien = value;
+            }
+        }
+    }
+}
