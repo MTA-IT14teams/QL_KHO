20b97f04c12f3b027e6cf8a01870645232cf6062
= <=>
2018-04-18 14:55:41 +0700

    model

---
+++ QL_KHO/QL_KHO/Controller/NCC_Controller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using QL_KHO.Model;
+using QL_KHO.Controller;
+
+namespace QL_KHO.Controller
+{
+    class NCC_Controller
+    {
+        SqlConnection conn = new SqlConnection(Controller.ConnectDatabase.ConnectionString);
+
+
+
+    }
+}
