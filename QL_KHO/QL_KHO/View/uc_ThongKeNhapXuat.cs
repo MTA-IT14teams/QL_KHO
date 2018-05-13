ac437845e2779f170125df1c3b2caa5a0d331bc5
Thuy Chinh <->
2018-05-14 00:13:58 +0700

    sua

--- QL_KHO/QL_KHO/View/uc_ThongKeNhapXuat.cs
+++ QL_KHO/QL_KHO/View/uc_ThongKeNhapXuat.cs
@@ -18,28 +18,28 @@ namespace QL_KHO.View
 
         private void Load_chart_Click(object sender, EventArgs e)
         {
-            using(ChartEntities db = new ChartEntities())
-            {
-                this.chart1.DataSource = db.PhieuNhaps.ToList();
-                this.chart1.Series["HangNhap"].XValueMember = "ngayXuat";
-                this.chart1.Series["HangNhap"].XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
-                this.chart1.Series["HangNhap"].YValueMembers = "tongTien";
-                this.chart1.Series["HangNhap"].YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            //using(ChartEntities db = new ChartEntities())
+            //{
+            //    this.chart1.DataSource = db.PhieuNhaps.ToList();
+            //    this.chart1.Series["HangNhap"].XValueMember = "ngayXuat";
+            //    this.chart1.Series["HangNhap"].XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            //    this.chart1.Series["HangNhap"].YValueMembers = "tongTien";
+            //    this.chart1.Series["HangNhap"].YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
 
-            }
+            //}
         }
 
         private void uc_ThongKeNhapXuat_Load(object sender, EventArgs e)
         {
-            using (ChartEntities db = new ChartEntities())
-            {
-                this.chart1.DataSource = db.PhieuNhaps.ToList();
-                this.chart1.Series["HangNhap"].XValueMember = "ngayXuat";
-                this.chart1.Series["HangNhap"].XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
-                this.chart1.Series["HangNhap"].YValueMembers = "tongTien";
-                this.chart1.Series["HangNhap"].YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            //using (ChartEntities db = new ChartEntities())
+            //{
+            //    this.chart1.DataSource = db.PhieuNhaps.ToList();
+            //    this.chart1.Series["HangNhap"].XValueMember = "ngayXuat";
+            //    this.chart1.Series["HangNhap"].XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            //    this.chart1.Series["HangNhap"].YValueMembers = "tongTien";
+            //    this.chart1.Series["HangNhap"].YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
 
-            }
+            //}
             
         }
     }
